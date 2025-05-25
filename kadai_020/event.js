f669c21c2d335bcfbf8529ced6eddbbecdfe8b42
jaykay0207 <jaykay0207@gmail.com>
2025-05-25 21:26:05 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,6 +0,0 @@
-const textElement = document.getElementById('text');
-const buttonElement = document.getElementById('btn');
-
-buttonElement.addEventListener('click', () => {
-  textElement.textContent = 'ボタンをクリックしました';
-});
